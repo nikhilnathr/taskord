fd2bd92ae99caf37a66055edd3e2177c0e4d9308
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-07-23 11:49:37 +0000

    Add conditional css for dark mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,11 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" data-turbolinks-track="true">
+    @if(Auth::check() && Auth::user()->isPatron)
+    @if(Auth::user()->darkMode)
+    <link href="{{ asset('css/darkmode.css') }}" rel="stylesheet" data-turbolinks-track="true">
+    @endif
+    @endif
     @livewireStyles
 </head>
 <body>
