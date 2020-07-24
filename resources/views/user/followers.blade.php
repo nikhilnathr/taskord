5d2cc81c355dc6a83224d847f075d7541e6efa74
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-07-24 02:52:08 +0000

    Fix user merging issue in connections page

--- resources/views/user/followers.blade.php
+++ resources/views/user/followers.blade.php
@@ -7,26 +7,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
-                    @foreach($user->followers as $user)
-                    <div class="card mb-3">
-                        <div class="card-body d-flex list-group-item align-items-center">
-                        <img class="rounded-circle avatar-40 mt-1" src="{{ $user->avatar }}" />
-                            <span class="ml-3">
-                                <a href="{{ route('user.done', ['username' => $user->username]) }}" class="align-text-top text-dark">
-                                    <span class="font-weight-bold">
-                                        {{ $user->firstname ? $user->firstname . ' ' . $user->lastname : $user->username }}
-                                    </span>
-                                    <div class="mb-2">{{ $user->bio }}</div>
-                                </a>
-                                @if (Auth::check() && Auth::user()->id !== $user->id && !$user->isFlagged)
-                                    @livewire('user.follow', [
-                                        'user' => $user
-                                    ])
-                                @endif
-                            </span>
-                        </div>
-                    </div>
-                    @endforeach
+                    @livewire('user.followers', ['user' => $user])
                 </div>
                 @include('user.sidebar')
             </div>
