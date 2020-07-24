674f33ef4b566f79023a91a5cdf7c54b05f04a16
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-07-24 12:18:13 +0000

    Remove Barrel roll hotkey 😂

--- resources/js/shortcuts.js
+++ resources/js/shortcuts.js
@@ -22,12 +22,6 @@ hotkeys('d+m', () => {
   });
 });
 
-// Do a barrel roll
-hotkeys('b+r', () => {
-  $('body').addClass('barrel_roll');
-  setTimeout("$('body').removeClass('barrel_roll')", 4000);
-});
-
 // Go to home
 hotkeys('g+h', () => {
   window.location.href = "/";
