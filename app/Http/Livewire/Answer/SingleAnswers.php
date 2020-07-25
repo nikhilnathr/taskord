bb19654d04254926a9dac34931e230d86525433e
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-07-25 05:36:22 +0000

    Add Question and Answer Livewire

---
+++ app/Http/Livewire/Answer/SingleAnswers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Answer;
+
+use Livewire\Component;
+
+class SingleAnswers extends Component
+{
+    public function render()
+    {
+        return view('livewire.answer.single-answers');
+    }
+}
---
+++ app/Http/Livewire/Question/SingleQuestions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Question;
+
+use Livewire\Component;
+
+class SingleQuestions extends Component
+{
+    public function render()
+    {
+        return view('livewire.question.single-questions');
+    }
+}
