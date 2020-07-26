ff794b4b171def4b039ce8ae8baaada4aeccba37
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-07-26 07:15:55 +0000

    Add Question Sidebar

--- resources/views/question/question.blade.php
+++ resources/views/question/question.blade.php
@@ -23,9 +23,35 @@
                             Asked by
                         </div>
                         <div class="card-body d-flex align-items-center">
-                            
+                            <img class="rounded-circle avatar-40 mt-1" src="{{ $question->user->avatar }}" />
+                            <span class="ml-3">
+                                <a href="{{ route('user.done', ['username' => $question->user->username]) }}" class="align-text-top text-dark">
+                                    <span class="font-weight-bold">
+                                        {{ $question->user->firstname ? $question->user->firstname . ' ' . $question->user->lastname : $question->user->username }}
+                                    </span>
+                                    <div>{{ $question->user->bio }}</div>
+                                </a>
+                            </span>
                         </div>
                     </div>
+                    @if ($question->answer->count() > 0)
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            Users Involved
+                        </div>
+                        <div class="card-body align-items-center">
+                            @foreach ($question->answer as $answer)
+                            <a
+                                title="{{ $answer->user->firstname ? $answer->user->firstname . ' ' . $answer->user->lastname : $answer->user->username }}"
+                                href="{{ route('user.done', ['username' => $answer->user->username]) }}"
+                                class="mr-1"
+                            >
+                                <img class="rounded-circle avatar-30 mb-2" src="{{ $answer->user->avatar }}" />
+                            </a>
+                            @endforeach
+                        </div>
+                    </div>
+                    @endif
                     <div class="text-black-50">
                         <span>
                             © Taskord
