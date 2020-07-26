6a10ef03097e9cfc221614186d93073dc7ac0d62
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-07-26 09:40:13 +0000

    Add versioned build for assets in prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,11 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/darkmode.scss', 'public/css');
+
+if(mix.inProduction()) {
+    mix.purgeCss()
+        .version();
+}
