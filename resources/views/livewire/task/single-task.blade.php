9a2da0c6ed911448deae6c9c74cab4c658caebf8
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-07-28 02:30:35 +0000

    Remove comment count if it is 0 in tasks

--- resources/views/livewire/task/single-task.blade.php
+++ resources/views/livewire/task/single-task.blade.php
@@ -86,14 +86,14 @@
                     </span>
                 </a>
             @endguest
-            @if ($task->task_comment->count() > 0)
             <a href="{{ route('task', ['id' => $task->id]) }}" class="btn btn-task btn-outline-primary mr-1">
                 {{ Emoji::speechBalloon() }}
+                @if ($task->task_comment->count() !== 0)
                 <span class="small text-dark font-weight-bold">
                     {{ $task->task_comment->count() }}
                 </span>
+                @endif
             </a>
-            @endif
         </div>
     </div>
 </li>
