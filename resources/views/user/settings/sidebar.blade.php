19ba9423444141f03e25f488fa295d3c7bac86a8
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-08-02 06:00:04 +0000

    Fix sidebar for dark mode

--- resources/views/user/settings/sidebar.blade.php
+++ resources/views/user/settings/sidebar.blade.php
@@ -4,19 +4,31 @@
             Settings
         </div>
         <ul class="list-group list-group-flush">
-            <a class="list-group-item list-group-item-action pt-2 pb-2" href="{{ route('user.settings.profile') }}">
+            <a
+                class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'user.settings.profile') active text-white @endif"
+                href="{{ route('user.settings.profile') }}"
+            >
                 <i class="fa fa-user mr-1"></i>
                 Profile
             </a>
-            <a class="list-group-item list-group-item-action pt-2 pb-2" href="{{ route('user.settings.account') }}">
+            <a
+                class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'user.settings.account') active text-white @endif"
+                href="{{ route('user.settings.account') }}"
+            >
                 <i class="fa fa-at mr-1"></i>
                 Account
             </a>
-            <a class="list-group-item list-group-item-action pt-2 pb-2" href="{{ route('user.settings.password') }}">
+            <a
+                class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'user.settings.password') active text-white @endif"
+                href="{{ route('user.settings.password') }}"
+            >
                 <i class="fa fa-key mr-1"></i>
                 Password
             </a>
-            <a class="list-group-item list-group-item-action text-danger pt-2 pb-2" href="{{ route('user.settings.delete') }}">
+            <a
+                class="list-group-item text-danger pt-2 pb-2 @if (Route::currentRouteName() === 'user.settings.delete') bg-danger text-white @endif"
+                href="{{ route('user.settings.delete') }}"
+            >
                 <i class="fa fa-exclamation-triangle mr-1"></i>
                 Danger Zone
             </a>
