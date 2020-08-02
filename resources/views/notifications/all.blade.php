193694ba9ccbf656d7733b9726c625c7415cfaf6
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-08-02 13:41:46 +0000

    Fix notification status button thanks @johnletey

--- resources/views/notifications/all.blade.php
+++ resources/views/notifications/all.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-8">
-            <a type="button" class="btn btn-primary mb-3" href="{{ route('notifications.unread') }}">
-                Unread 
+            <a role="button" class="btn btn-primary mb-3" href="{{ route('notifications.unread') }}">
+                Unread
                 <span class="ml-1 badge bg-white text-black-50">
                     {{ Auth::user()->unreadNotifications->count() }}
                 </span>
             </a>
-            <a type="button" class="btn btn-primary mb-3 mr-2" href="{{ route('notifications.all') }}">
+            <a role="button" class="btn btn-primary mb-3 mr-2" href="{{ route('notifications.all') }}">
                 All
             </a>
             @if (Auth::user()->notifications->count() !== 0)
