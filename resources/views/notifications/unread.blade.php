de83210aca20bce6c7a26a4ce51a0186c187989f
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-08-02 13:54:19 +0000

    Update notification status buttons

--- resources/views/notifications/unread.blade.php
+++ resources/views/notifications/unread.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-8">
-            <a type="button" class="btn btn-primary mb-3" href="{{ route('notifications.unread') }}">
+            <a role="button" class="btn btn-primary mb-3" href="{{ route('notifications.unread') }}">
                 Unread
                 <span class="ml-1 badge bg-white text-black-50">
                     {{ Auth::user()->unreadNotifications->count() }}
                 </span>
             </a>
-            <a type="button" class="btn btn-primary mb-3 mr-2" href="{{ route('notifications.all') }}">
+            <a role="button" class="btn btn-primary mb-3 mr-2" href="{{ route('notifications.all') }}">
                 All
             </a>
             @if (Auth::user()->unreadNotifications->count() !== 0)
