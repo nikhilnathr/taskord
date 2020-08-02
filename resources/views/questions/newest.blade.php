3d7a5d8c8fbb0c686f60b3c0738437e08acee5b3
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-08-02 11:42:34 +0000

    Add Nav as separate component in questions page

--- resources/views/questions/newest.blade.php
+++ resources/views/questions/newest.blade.php
@@ -6,21 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
-                    <div class="mb-3">
-                        <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.newest' ? '' : 'outline-' }}primary mr-2" href="{{ route('questions.newest') }}">
-                            Newest
-                        </a>
-                        <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.unanswered' ? '' : 'outline-' }}primary mr-2" href="{{ route('questions.unanswered') }}">
-                            Unanswered
-                        </a>
-                        <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.popular' ? '' : 'outline-' }}primary mr-2" href="{{ route('questions.popular') }}">
-                            Popular
-                        </a>
-                        <a class="btn btn-success float-md-right mb-2 text-white" href="{{ route('questions.new') }}">
-                            <i class="fa fa-plus"></i>
-                            New Question
-                        </a>
-                    </div>
+                    @include('questions.nav')
                     @if (session()->has('question_deleted'))
                         <div class="alert alert-success alert-dismissible fade show mt-2">
                             <button type="button" class="close" data-dismiss="alert">×</button>
