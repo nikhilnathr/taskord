cecd2154043a7646d464828e7dd82bb7c3239dce
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-08-02 11:41:13 +0000

    Add Nav as separate component in products page

--- resources/views/products/launched.blade.php
+++ resources/views/products/launched.blade.php
@@ -6,18 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
-                    <div class="mb-3">
-                        <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'products.newest' ? '' : 'outline-' }}primary mr-2" href="{{ route('products.newest') }}">
-                            Newest
-                        </a>
-                        <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'products.launched' ? '' : 'outline-' }}primary mr-2" href="{{ route('products.launched') }}">
-                            Launched
-                        </a>
-                        <a class="mb-2 btn btn-success float-md-right text-white" href="{{ route('products.new') }}">
-                            <i class="fa fa-plus"></i>
-                            New Product
-                        </a>
-                    </div>
+                    @include('products.nav')
                     @livewire('products.products', [
                         'type' => 'products.launched',
                         'page' => 1,
--- resources/views/products/newest.blade.php
+++ resources/views/products/newest.blade.php
@@ -13,18 +13,7 @@
                             {{ session('product_deleted') }}
                         </div>
                     @endif
-                    <div class="mb-3">
-                        <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'products.newest' ? '' : 'outline-' }}primary mr-2" href="{{ route('products.newest') }}">
-                            Newest
-                        </a>
-                        <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'products.launched' ? '' : 'outline-' }}primary mr-2" href="{{ route('products.launched') }}">
-                            Launched
-                        </a>
-                        <a class="mb-2 btn btn-success float-md-right text-white" href="{{ route('products.new') }}">
-                            <i class="fa fa-plus"></i>
-                            New Product
-                        </a>
-                    </div>
+                    @include('products.nav')
                     @livewire('products.products', [
                         'type' => 'products.newest',
                         'page' => 1,
