537399e8041f7508bd2aa15199dcea7433ea56eb
Dabbit <68646469+dabbit@users.noreply.github.com>
2020-08-02 06:08:05 +0000

    Hide notification count when it is 0 and Closes #182

--- resources/views/livewire/notification/icon.blade.php
+++ resources/views/livewire/notification/icon.blade.php
@@ -1,8 +1,10 @@
-<li class="nav-item mr-2">
+<li wire:poll class="nav-item mr-2">
     <a class="nav-link text-white" href="{{ route('notifications') }}">
         {{ Emoji::bell() }}
-        <span wire:poll class="notification-count bg-danger font-weight-bold rounded">
+        @if (Auth::user()->notifications->count() !== 0)
+        <span class="notification-count bg-danger font-weight-bold rounded">
             {{ Auth::user()->notifications->count() }}
         </span>
+        @endif
     </a>
 </li>
